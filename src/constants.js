3efe8c03ff2e375db4328f5faefdcd66cd065999
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2020-05-10 03:06:34 +0300

    Adds missing constants

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const tools = "/tools";
+export const root = "/";
+export const inputId = "fileInputDad";
+export const dadFileUploadURL = "/daduploadfile/";
+export const backendURL = "http://127.0.0.1:8080";
+export const FETCH_STATES = {
+  NOT_STARTED: 1,
+  STARTED: 2,
+  ERROR: 3,
+  SUCCESS: 4
+};
