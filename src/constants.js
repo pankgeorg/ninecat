6f45d808a6d28e2131d266e296d46dd1e567a5a6
Panagiotis Georgakopoulos <pankgeorg@gmail.com>
2020-09-22 07:44:33 +0300

    Add hasura, update to pankgeorg.com

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const pharma = "/pharma";
 export const root = "/";
 export const inputId = "fileInputDad";
 export const dadFileUploadURL = "/daduploadfile/";
-export const backendURL = "https://api.silentech.gr";
+export const backendURL = "https://api.pankgeorg.com";
 export const FETCH_STATES = {
   NOT_STARTED: 1,
   STARTED: 2,
